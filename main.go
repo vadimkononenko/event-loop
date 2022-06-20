04065b450b1b92466eaad138b35c201d6cc3c8bc
vadimkononenko <vadiskon@gmail.com>
2022-06-20 13:57:54 +0300

    feature: main.go, input.txt

--- main.go
+++ main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"github.com/vadimkononenko/event-loop/engine"
+	"os"
 )
 
 func main() {
 	eventLoop := new(engine.EventLoop)
 	eventLoop.Start()
 
-	//if input, err := os.Open(inputFile); err == nil {
-	//	defer input.Close()
-	//	scanner := bufio.NewScanner(input)
-	//	for scanner.Scan() {
-	//		commandLine := scanner.Text()
-	//		cmd := parse(commandLine) // parse the line to get a Command
-	//		eventLoop.Post(cmd)
-	//	}
-	//}
+	if input, err := os.Open("input.txt"); err == nil {
+		defer input.Close()
+		scanner := bufio.NewScanner(input)
+		for scanner.Scan() {
+			commandLine := scanner.Text()
+			cmd := engine.Parse(commandLine) // parse the line to get a Command -> &printCommand{arg: “hello”}
+			eventLoop.Post(cmd)
+		}
+	}
 
 	eventLoop.AwaitFinish()
 }
